4f0cf3f6ca8903173cff160dcf6d2b8c0f83614e
Bernardo Meirelles <contato@bernardomrl.dev>
2026-04-14 10:22:12 -0300

    feat: refactor base layout structure
    - restructure layout.tsx
    - add global font and base tailwind classes to body
    - remove default page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,24 @@
-import type { Metadata } from 'next';
-import type { PropsWithChildren } from 'react';
+import { Inter as Sans } from 'next/font/google';
+
+import { RootProvider } from '@/components/providers';
+import { cn } from '@/lib/utils';
 
 import '@/styles/globals.css';
 
-export const metadata: Metadata = {
-	title: {
-		default: 'Next.js boilerplate',
-		template: '%s | Next.js'
-	}
-};
+const sans = Sans({ subsets: ['latin'], variable: '--font-sans', display: 'swap' });
 
-export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
+export default function RootLayout({ children }: Readonly<React.PropsWithChildren>) {
 	return (
-		<html lang="pt-BR">
-			<body className="font-sans antialiased">{children}</body>
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body
+				className={cn(
+					'text-foreground bg-background font-sans subpixel-antialiased',
+					sans.variable
+				)}
+			>
+				<RootProvider>{children}</RootProvider>
+			</body>
 		</html>
 	);
 }
